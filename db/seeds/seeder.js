26a9011a6f5d2032ee3e18a320e2df39f998bff3
Hardronox <Sanya.Chuck@mail.ru>
2018-04-06 17:58:20 +0300

    seeder

--- db/seeds/seeder.js
+++ db/seeds/seeder.js
@@ -2,19 +2,34 @@ const mongoose = require('mongoose');
 const User = require('../../models/user.js');
 const Faker = require('faker');
 
+class Seeder {
 
-mongoose.Promise = global.Promise;
-const db = mongoose.connect('mongodb://localhost/store');
+  constructor(seeder) {
+    mongoose.Promise = global.Promise;
+    this.db = mongoose.connect('mongodb://localhost/store');
 
-const docs_amount = 50;
+    this.seeder = seeder;
+  }
 
-for (let i=0; i < docs_amount; i++) {
-  User.create({
-    firstName: Faker.name.firstName(),
-    lastName: Faker.name.lastName(),
-    email: Faker.internet.email(),
-    password: 'asdfasdfasdfasdfasdfasdf',
-  }).then(console.log('dane'));
+  run() {
+    //this.seeder[2]
+  }
+
 }
 
-db.connection.close();
+
+// const docs_amount = 50;
+//
+// for (let i=0; i < docs_amount; i++) {
+//   User.create({
+//     firstName: Faker.name.firstName(),
+//     lastName: Faker.name.lastName(),
+//     email: Faker.internet.email(),
+//     password: 'asdfasdfasdfasdfasdfasdf',
+//   }).then(console.log('dane'));
+// }
+
+
+const seeder = new Seeder(process.argv);
+
+seeder.run();
